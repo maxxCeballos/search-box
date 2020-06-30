32acd7b2e02b5d7699bdbb410b806a0aee6cf6fb
Maxx <maxic2405@gmail.com>
2020-06-30 20:23:25 -0300

    asldkasd

--- client/rxsearch.js
+++ client/rxsearch.js
@@ -1,5 +1,17 @@
 'use strict'
 
+
+// import { range } from "rxjs";
+// import { map, filter } from "rxjs/operators";
+// import * as Rx from 'rxjs/Rx';
+
+// import { Observable } from 'rxjs/Observable';
+
+
+const { Observable, fromEvent, from } = rxjs;
+// import 'rxjs/add/observable/fromPromise';
+// import { from, Observable } from 'rxjs';
+
 const searchBox = document.getElementById('search');
 const results = document.getElementById('results');
 
@@ -13,6 +25,25 @@ let testData = [
     'https://www.manning.com/books/rxjs-in-action'
 ];
 
+// const myObservable = fromEvent(searchBox, 'keyup');
+// myObservable.subscribe((data) => {
+//     // content.textContent = moment().format('LTS');
+//     console.log('data ', data);
+// });
+
+
+const lala = Observable.from(fetch('http://localhost:3000/'))
+   
+lala.subscribe(data => console.log('la data de fetch ', data));
+
+
+// fetch('http://localhost:3000/')
+// .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(myJson) {
+//     console.log(myJson);
+//   });
 
 searchBox.addEventListener('keyup', function (event) {
     let query = event.target.value;
@@ -38,7 +69,7 @@ function clearResults(container) {
 }
 
 
-// agrega resultados a la vista
+// agrega resultados a la vista html
 function appendResults(result, container) {
     let li = document.createElement('li');
     let text = document.createTextNode(result);
--- server/index.js
+++ server/index.js
@@ -1,3 +1,33 @@
 'use strict'
 
+const express = require('express');
+const app = express();
 
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+  }
+
+app.use(allowCrossDomain);
+
+
+app.get('/', (req, res) => {
+    
+    let testData = [
+        'github.com/Reactive-Extensions/RxJS',
+        'github.com/ReactiveX/RxJS',
+        'xgrommx.github.io/rx-book',
+        'reactivex.io',
+        'egghead.io/technologies/rx',
+        'rxmarbles.com',
+        'https://www.manning.com/books/rxjs-in-action'
+    ];
+
+    res.send(testData);
+
+})
+
+
+
+app.listen( 3000, () => console.log('Server up'));
