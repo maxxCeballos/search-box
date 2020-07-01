63af4bd24d5ab7dd38bf23afafdd26b248c4a793
Nahir Ivonne Saddi Beraud <Nahir@PRACTIAARNB1084.pragmanet.local>
2020-07-01 01:45:10 -0300

    mejora de estilo y un poco mas

--- client/rxsearch.js
+++ client/rxsearch.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// import 'rxjs/add/observable/fromPromise';
+// import { from, Observable } from 'rxjs';
 
 // import { range } from "rxjs";
 // import { map, filter } from "rxjs/operators";
@@ -9,11 +11,63 @@
 
 
 const { Observable, fromEvent, from } = rxjs;
-// import 'rxjs/add/observable/fromPromise';
-// import { from, Observable } from 'rxjs';
 
+const bars_search = document.getElementById("ctn-bars-search");
+const cover_ctn_search =  document.getElementById("cover-ctn-search");
 const searchBox = document.getElementById('search');
 const results = document.getElementById('results');
+
+
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+function mostrar_menu(){
+    document.getElementById("move-content").classList.toggle('move-container-all');
+    document.getElementById("show-menu").classList.toggle('show-lateral');
+}
+
+document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+//Funcion para mostrar el buscador
+function mostrar_buscador(){
+    bars_search.style.top = "80px";
+    cover_ctn_search.style.display = "block";
+    searchBox.addEventListener('keyup', function (event) {
+        let query = event.target.value;
+        let searchResults = [];
+        if(query && query.length > 0) {
+            console.log(query);
+            clearResults(results);
+           for(let result of testData) {
+             if(result.startsWith(query)) {
+               searchResults.push(result);
+     } }
+        }
+        if (searchBox.value === ""){
+            console.log("aaaaaa")
+           results.style.display = "none";
+       }else{
+        results.style.display = "block";
+       }
+     
+        for(let result of searchResults) {
+           appendResults(result, results);
+        }
+     });
+    
+   
+}
+
+document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
+//Funcion para ocultar el buscador
+function ocultar_buscador(){
+    bars_search.style.top = "-10px";
+    cover_ctn_search.style.display = "none";
+    searchBox.value = "";
+    console.log("bvvvvvvvv")
+   results.style.display = "none";
+
+}
+
+
+
 
 let testData = [
     'github.com/Reactive-Extensions/RxJS',
@@ -45,21 +99,6 @@ let testData = [
 //     console.log(myJson);
 //   });
 
-searchBox.addEventListener('keyup', function (event) {
-    let query = event.target.value;
-    let searchResults = [];
-    if(query && query.length > 0) {
-        clearResults(results);
-       for( let result of testData) {
-         if(result.startsWith(query)) {
-           searchResults.push(result);
- } }
-    }
-    for(let result of searchResults) {
-       appendResults(result, results);
-    }
- });
-
 
 // limpiar los resultados de busqueda
 function clearResults(container) {
@@ -72,7 +111,16 @@ function clearResults(container) {
 // agrega resultados a la vista html
 function appendResults(result, container) {
     let li = document.createElement('li');
+    let i = document.createElement('i');
+    i.setAttribute("class", "fas fa-search");
+    li.appendChild(i);
     let text = document.createTextNode(result);
     li.appendChild(text);
+    console.log("conta",container);
     container.appendChild(li);
+    
 }
+
+
+
+
